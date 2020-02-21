5a23065863a4adade9d2c808e0ecb7b396bbd152
anaarezo <laura.arezo@gmail.com>
2020-02-21 15:23:43 -0300

    001-Part1 Feat: First

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+const resolvers ={
+    Query: {
+        hello: () => "Hello World",
+    },
+};
+
+//Omitir informações da propriedade ex.: rest
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () => {
+    console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
+});
